12387274303a7f5108e1d11d3b97fbc99a95e4ff
Danny Navejas <dannynavejas@gmail.com>
2019-05-20 21:32:05 -0500

    added tests for moving forward functions for west, north and south

--- tests/driving-tests.js
+++ tests/driving-tests.js
@@ -8,10 +8,6 @@ describe("Driving a Car", function() {
     beforeEach(function() {
       // create a sandbox
       sandbox = sinon.sandbox.create();
-
-      // stub some console methods
-      //sandbox.stub(window.console, "log");
-      //sandbox.stub(window.console, "error");
         
         car = {};
             car.style = {};
@@ -19,7 +15,7 @@ describe("Driving a Car", function() {
             car.style.left = "";
             car.className = "";
 
-        //mocing complex objects such as DOM
+        //mocking complex objects such as DOM
             //https://codeutopia.net/blog/2016/05/23/sinon-js-quick-tip-how-to-stubmock-complex-objects-such-as-dom-objects/
 
         //mocking document load
@@ -51,68 +47,61 @@ describe("Driving a Car", function() {
 
     describe("calling forward while facing west", function() {
         it("should move car from left to right", function() {
-        
+            //SETUP
+            car.style.top = "0px";
+            car.style.left = "0px";
+            car.className = "car west";
+
+            //ACT
+            forward(car);
+
+            //ASSERT
+            expect(getDirection(car)).to.equal("WEST");
+            expect(car.style.top).to.equal('0px');
+            expect(car.style.left).to.equal('-10px');
         });
     });
 
     describe("calling forward while facing north", function() {
         it("should move car from bottom to top", function() {
-        
+            //SETUP
+            car.style.top = "0px";
+            car.style.left = "0px";
+            car.className = "car north";
+
+            //ACT
+            forward(car);
+
+            //ASSERT
+            expect(getDirection(car)).to.equal("NORTH");
+            expect(car.style.top).to.equal('-10px');
+            expect(car.style.left).to.equal('0px');
         });
     });
 
     describe("calling forward while facing south", function() {
         it("should move car from top to bottom", function() {
-        
-        });
+            //SETUP
+            car.style.top = "0px";
+            car.style.left = "0px";
+            car.className = "car south";
+
+            //ACT
+            forward(car);
+
+            //ASSERT
+            expect(getDirection(car)).to.equal("SOUTH");
+            expect(car.style.top).to.equal('10px');
+            expect(car.style.left).to.equal('0px');
+            });
     });
 
-/// EXAMPLE SYNTAX BELOW
-
-//   describe("#greets", function() {
-//     it("should throw if no target is passed in", function() {
-//       expect(function() {
-//         (new Cow()).greets();
-//       }).to.throw(Error);
-//     });
-
-//     it("should greet passed target", function() {
-//       var greetings = (new Cow("Kate")).greets("Baby");
-//       expect(greetings).to.equal("Kate greets Baby");
-//     });
-//   });
-
-//   describe("#consoleGreets", function() {
-//     it("should throw if no target is passed in", function() {
-//         (new Cow()).consoleGreets();
-
-//         sinon.assert.notCalled(console.log);
-//         sinon.assert.calledOnce(console.error);
-//         sinon.assert.calledWithExactly(console.error, "missing target");
-//     });
-
-//     it("should greet passed target", function() {
-//       var greetings = (new Cow("Kate")).consoleGreets("Baby");
-
-//       sinon.assert.notCalled(console.error);
-//       sinon.assert.calledOnce(console.log);
-//       sinon.assert.calledWithExactly(console.log, "Kate greets Baby")
-//     });
-//   });
-
-//   describe("#lateGreets", function() {
-//     it("should pass an error if no target is passed", function(done) {
-//       (new Cow()).lateGreets(null, function(err, greetings) {
-//         expect(err).to.be.an.instanceof(Error);
-//         done();
-//       });
-//     });
-
-//     it("should greet passed target after one second", function(done) {
-//       (new Cow("Kate")).lateGreets("Baby", function(err, greetings) {
-//         expect(greetings).to.equal("Kate greets Baby");
-//         done();
-//       });
-//     });
-//   });
-});
+describe("Adding a Car", function(){
+    let sandbox;
+    let car;
+    
+    beforeEach(function(){
+        sandbox = sinon.sanbox.create();
+        
+    })
+})})
